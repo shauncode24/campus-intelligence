f4391317187ab789b6019c87c8fc325610d435c2
shauncode24 <shaunak.karve@somaiya.edu>
2025-12-21 22:24:38 +0530

    Homepage herosection

--- frontend/src/pages/Homepage.jsx
+++ frontend/src/pages/Homepage.jsx
@@ -5,7 +5,23 @@ export default function Homepage() {
   return (
     <>
       <Header />
-      {/* <div className="default homepage-hero-main"></div> */}
+      <div className="default homepage-hero-main">
+        <div className="default homepage-hero-title">
+          Official answers from <span>official documents</span>
+        </div>
+        <div className="default homepage-hero-sub-title">
+          Stop guessing. Get accurate, instant answers about campus policies,
+          deadlines, and procedures directly from the source.
+        </div>
+        <div className="default homepage-hero-button-div">
+          <button className="default homepage-hero-button-question">
+            Ask a Question
+          </button>
+          <button className="default homepage-hero-button-login">
+            Admin Login
+          </button>
+        </div>
+      </div>
     </>
   );
 }
